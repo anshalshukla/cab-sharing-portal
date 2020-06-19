a8f7ddea1a724607292879d2283295dc2b740185
anshalshukla <shukla.anshal85@gmail.com>
2020-06-19 12:23:36 +0530

    CORS enabled.

--- server/src/app.js
+++ server/src/app.js
@@ -17,6 +17,7 @@ const typeDefs = require("./graphql/typedefs");
 const resolvers = require("./graphql/resolvers");
 
 const server = new ApolloServer({
+    cors: true,
     typeDefs,
     resolvers,
     introspection: true,
